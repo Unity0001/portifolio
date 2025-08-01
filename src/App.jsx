e49a01bf70d359fe8dcd2971abeb3fc641a02400
Unity <guilhermtrout@gmail.com>
2025-08-01 19:59:45 -0300

    Ajustes finais de responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import HeroSection from "./components/heroSection";
 
 function App() {
-  return (
-    <div className="relative h-screen ml-8">
-      <HeroSection />
-    </div>
-  );
+  return <HeroSection />;
 }
 
 export default App;

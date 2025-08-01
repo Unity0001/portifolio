95d6c978ae0127c64e558d5faf20197137581c2d
Unity <guilhermtrout@gmail.com>
2025-08-01 19:55:44 -0300

    Ajustes finais

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import HeroSection from "./components/heroSection";
 
 function App() {
   return (
-    <div className="relative h-screen mx-16">
+    <div className="relative h-screen ml-8">
       <HeroSection />
     </div>
   );
